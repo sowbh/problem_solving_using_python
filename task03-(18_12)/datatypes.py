bc891f1def78a30d595146ebb4bb5fef5f3b9991
Sowbhagya Janardhan <sowbhagyaj30@gmail.com>
2025-05-26 11:24:00 +0530

    python intro

---
+++ task03-(18_12)/datatypes.py
@@ -0,0 +1,50 @@
+# numeric;
+## integer;
+num1 = 10;
+print(num1, type(num1));
+
+## complex;
+num2 = 3+6j;
+print(num2, type(num2));
+
+## float;
+num3 = 4.5;
+print(num3, type(num3));
+
+#-----
+
+# boolean(True or False)
+present = True;
+print(present, type(present));
+
+#-----
+
+# dictionary
+obj1 = {1:'A', 2:'B', 3:'C'};
+print(obj1, type(obj1));
+
+#-----
+
+# set (only unique values)
+l1 = {2,3,2,3};
+print(l1, type(l1));
+
+#-----
+
+# sequence type;
+## list(same as arrays)
+l2 = [2, 4.1, "hello"];
+print(l2, type(l2));
+
+#-----
+
+## tuple (tuples cannot be changed)
+t1 = (2, 4, 5, "hi");
+print(t1, type(t1));
+
+#-----
+
+## strings
+str1 = "This is Python";
+print(str1);
+
---
+++ task03-(18_12)/sum_of_digits.py
@@ -0,0 +1,14 @@
+# in js;
+# num = prompt("Enter a number: ");  #1234 in strings because of "prompt"
+# sum=0;
+# for(i=0; i<=num; i++){
+#     sum+=Number(num[i]);   #Number-str to num
+# }
+# console.log(sum);
+
+#------
+
+# in python;
+num = [int(i) for i in input("Enter a number:")];
+print(sum(num));
+
